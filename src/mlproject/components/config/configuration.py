d4d15163a78da9d8a84c6807fec6461ffcabe897
Abhishek Singh <abhisheksingh050530@gmail.com>
2025-05-12 00:32:18 +0530

    model

--- src/mlproject/components/config/configuration.py
+++ src/mlproject/components/config/configuration.py
@@ -3,10 +3,10 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifacts', 'train.csv')
-    test_data_path: str = os.path.join('artifacts', 'test.csv')
-    raw_data_path: str = os.path.join('artifacts', 'raw.csv')
+    train_data_path: str = os.path.join('artifact', 'train.csv')
+    test_data_path: str = os.path.join('artifact', 'test.csv')
+    raw_data_path: str = os.path.join('artifact', 'raw.csv')
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path: str = os.path.join('artifacts', 'model.pkl')
+    trained_model_file_path: str = os.path.join('artifact', 'model.pkl')
